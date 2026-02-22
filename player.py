7f7eaba2d932be6fdbe5ece4a45bfd6c00b96bf9
Julia <tjuliazhu@gmail.com>
2026-02-22 17:30:44 -0500

    formatting and merging

--- player.py
+++ player.py
@@ -30,15 +30,14 @@ class Player:
        self.init_x = x
        self.init_y = y
 
-   def touchwall(self, walls: list[Wall]) -> None:
-        for w in walls:
-            left_edge = w.x - w.width / 2 - self.width*(1/2)
-            right_edge = w.x + w.width / 2 + self.width*(1/2)
-            top_edge = w.y - w.height / 2 - self.width*(1/2)
-            bottom_edge = w.y + w.height / 2 + self.width*(1/2)
-            if left_edge < self.x < right_edge and top_edge < self.y < bottom_edge:
-                self.x == self.init_x 
-                self.y == self.init_y
+   def runinto(self, wall: Wall) -> None:
+        left_edge = wall.x - wall.width / 2 - self.width*(1/2)
+        right_edge = wall.x + wall.width / 2 + self.width*(1/2)
+        top_edge = wall.y - wall.height / 2 - self.width*(1/2)
+        bottom_edge = wall.y + wall.height / 2 + self.width*(1/2)
+        if left_edge < self.x < right_edge and top_edge < self.y < bottom_edge:
+            self.x = self.init_x 
+            self.y = self.init_y
 
 
    def update(self) -> None:
@@ -59,20 +58,11 @@ class Player:
         self.x = self.screen.get_width() - 0.5 * self.width
 
 
-    if self.y < 0.5 * self.height:
-        self.y = 0.5 * self.height
-    if self.y > self.screen.get_height() - 0.5 * self.height:
-        self.y = self.screen.get_height() - 0.5 * self.height
-    
- 
-        self.touchwall([Wall(self.screen, 100, 30, 300, 100),
-        Wall(self.screen, 600, 200, 50, 300),
-        Wall(self.screen, 100, 800, 50, 500),
-        Wall(self.screen, 1000, 50, 300, 800),
-        Wall(self.screen, 1200, 700, 400, 200),
-        Wall(self.screen, 600, 600, 400, 200),
-        Wall(self.screen, 300, 300, 200, 300)])
-        
+    if self.y < 0.7 * self.height:
+        self.y = 0.7 * self.height
+    if self.y > self.screen.get_height() - 0.7 * self.height:
+        self.y = self.screen.get_height() - 0.7* self.height
+
 
    def display(self) -> None:
        rect_x = self.x - self.width / 2
