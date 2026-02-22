52694d18401acaa78334e55ff9c82ce636270c19
Julia <tjuliazhu@gmail.com>
2026-02-22 17:24:41 -0500

    working on touch wall from class

--- player.py
+++ player.py
@@ -30,14 +30,15 @@ class Player:
        self.init_x = x
        self.init_y = y
 
-   def touchwall(self, wall: Wall) -> None:
-        left_edge = wall.x - wall.width / 2 - self.width*(1/2)
-        right_edge = wall.x + wall.width / 2 + self.width*(1/2)
-        top_edge = wall.y - wall.height / 2 - self.width*(1/2)
-        bottom_edge = wall.y + wall.height / 2 + self.width*(1/2)
-        if left_edge < self.x < right_edge and top_edge < self.y < bottom_edge:
-            self.x == self.init_x 
-            self.y == self.init_y
+   def touchwall(self, walls: list[Wall]) -> None:
+        for w in walls:
+            left_edge = w.x - w.width / 2 - self.width*(1/2)
+            right_edge = w.x + w.width / 2 + self.width*(1/2)
+            top_edge = w.y - w.height / 2 - self.width*(1/2)
+            bottom_edge = w.y + w.height / 2 + self.width*(1/2)
+            if left_edge < self.x < right_edge and top_edge < self.y < bottom_edge:
+                self.x == self.init_x 
+                self.y == self.init_y
 
 
    def update(self) -> None:
@@ -63,21 +64,15 @@ class Player:
     if self.y > self.screen.get_height() - 0.5 * self.height:
         self.y = self.screen.get_height() - 0.5 * self.height
     
-    walls = [
-        Wall(self.screen, 100, 30, 300, 100),
+ 
+        self.touchwall([Wall(self.screen, 100, 30, 300, 100),
         Wall(self.screen, 600, 200, 50, 300),
-        Wall(screen, 100, 800, 50, 500),
-        Wall(screen, 1000, 50, 300, 800),
-        Wall(screen, 1200, 700, 400, 200),
-        Wall(screen, 600, 600, 400, 200),
-        Wall(screen, 300, 300, 200, 300)
-    ]
-
-    for w in walls:
-        self.touchwall()
-
+        Wall(self.screen, 100, 800, 50, 500),
+        Wall(self.screen, 1000, 50, 300, 800),
+        Wall(self.screen, 1200, 700, 400, 200),
+        Wall(self.screen, 600, 600, 400, 200),
+        Wall(self.screen, 300, 300, 200, 300)])
         
-
 
    def display(self) -> None:
        rect_x = self.x - self.width / 2
