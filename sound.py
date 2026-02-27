00fef47a384447681d40ab24b3e32f70138e9891
Esther <thetempleariel@gmail.com>
2026-02-27 14:05:53 -0500

    Sound

---
+++ sound.py
@@ -0,0 +1,16 @@
+import pygame
+from player import Player
+pygame.mixer.pre_init(44100, -16, 2, 2048) 
+pygame.init()
+
+ahsound = pygame.mixer.Sound("alarm.mp3")
+
+def alarmed(runner:Player, tagger: Player) -> bool:
+   left_edge = tagger.x - tagger.width / 2 - 4
+   right_edge = tagger.x + tagger.width / 2 + 4
+   top_edge = tagger.y - tagger.height / 2 - 4
+   bottom_edge = tagger.y + tagger.height / 2 + 4
+   if left_edge < runner.x < right_edge and top_edge < runner.y < bottom_edge:
+       ahsound.play()
+       return True
+   return False
